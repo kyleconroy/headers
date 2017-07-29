f1a08b0b36b96c2aeee21c2b782f24aecef4c6ce
Kyle Conroy <kyle@kyleconroy.com>
2017-07-29 01:58:23 -0700

    Finish documenting all the CORS headers

--- cors.go
+++ cors.go
@@ -46,7 +46,17 @@ func (h *AccessControlAllowCredentials) Parse(hdr string) error {
 
 var _ Header = &AccessControlAllowCredentials{}
 
+// The Access-Control-Max-Age response header indicates how long the results of
+// a preflight request (that is the information contained in the
+// Access-Control-Allow-Methods and Access-Control-Allow-Headers headers) can
+// be cached.
+//
+// https://mdn.io/Access-Control-Max-Age
 type AccessControlMaxAge struct {
+	// Maximum number of seconds the results can be cached. Firefox caps this at 24
+	// hours (86400 seconds) and Chromium at 10 minutes (600 seconds). Chromium
+	// also specifies a default value of 5 seconds. A value of -1 will disable
+	// caching, requiring a preflight OPTIONS check for all calls.
 	Age time.Duration
 }
 
@@ -69,6 +79,13 @@ func (h *AccessControlMaxAge) Parse(hdr string) error {
 
 var _ Header = &AccessControlMaxAge{}
 
+// The Access-Control-Request-Method request header is used when issuing a
+// preflight request to let the server know which HTTP method will be used when
+// the actual request is made. This header is necessary as the preflight
+// request is always an OPTIONS and doesn't use the same method as the actual
+// request.
+//
+// https://mdn.io/Access-Control-Request-Method
 type AccessControlRequestMethod struct {
 	Method string
 }
@@ -88,7 +105,13 @@ func (h *AccessControlRequestMethod) Parse(hdr string) error {
 
 var _ Header = &AccessControlRequestMethod{}
 
+// The Access-Control-Request-Headers request header is used when issuing a
+// preflight request to let the server know which HTTP headers will be used
+// when the actual request is made.
+//
+// https://mdn.io/Access-Control-Request-Headers
 type AccessControlRequestHeaders struct {
+	// A list of HTTP headers that are included in the request.
 	Headers []string
 }
 
@@ -107,6 +130,11 @@ func (h *AccessControlRequestHeaders) Parse(hdr string) error {
 
 var _ Header = &AccessControlRequestHeaders{}
 
+// The Access-Control-Allow-Methods response header specifies the method or
+// methods allowed when accessing the resource in response to a preflight
+// request.
+//
+// https://mdn.io/Access-Control-Allow-Methods
 type AccessControlAllowMethods struct {
 	Methods []string
 }
@@ -126,6 +154,20 @@ func (h *AccessControlAllowMethods) Parse(hdr string) error {
 
 var _ Header = &AccessControlAllowMethods{}
 
+// The Access-Control-Allow-Headers response header is used in response to a
+// preflight request to indicate which HTTP headers will be available via
+// Access-Control-Expose-Headers when making the actual request.
+//
+// The simple headers, Accept, Accept-Language, Content-Language, Content-Type
+// (but only with a MIME type of its parsed value (ignoring parameters) of
+// either application/x-www-form-urlencoded, multipart/form-data, or
+// text/plain), are always available and don't need to be listed by this
+// header.
+//
+// This header is required if the request has an Access-Control-Request-Headers
+// header.
+//
+// https://mdn.io/Access-Control-Allow-Headers
 type AccessControlAllowHeaders struct {
 	Headers []string
 }
@@ -145,6 +187,21 @@ func (h *AccessControlAllowHeaders) Parse(hdr string) error {
 
 var _ Header = &AccessControlAllowHeaders{}
 
+// The Access-Control-Expose-Headers response header indicates which headers
+// can be exposed as part of the response by listing their names.
+//
+// By default, only the 6 simple response headers are exposed:
+// - Cache-Control
+// - Content-Language
+// - Content-Type
+// - Expires
+// - Last-Modified
+// - Pragma
+//
+// If you want clients to be able to access other headers, you have to list
+// them using the Access-Control-Expose-Headers header.
+//
+// https://mdn.io/Access-Control-Expose-Headers
 type AccessControlExposeHeaders struct {
 	Headers []string
 }
@@ -162,7 +219,16 @@ func (h *AccessControlExposeHeaders) Parse(hdr string) error {
 	return nil
 }
 
+var _ Header = &AccessControlExposeHeaders{}
+
+// The Access-Control-Allow-Origin response header indicates whether the
+// response can be shared with resources with the given origin.
+//
+// https://mdn.io/Access-Control-Allow-Origin
 type AccessControlAllowOrigin struct {
+	// Specifies a URI that may access the resource. For requests without
+	// credentials, the server may specify "*" as a wildcard, thereby allowing
+	// any origin to access the resource.
 	Origin string
 }
 
