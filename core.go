7fd6114ff776a7d758cd0aca929d377173ac44e9
Kyle Conroy <kyle@kyleconroy.com>
2017-07-29 02:14:00 -0700

    Add the Age header

--- core.go
+++ core.go
@@ -1,12 +1,17 @@
 package headers
 
-import "net/url"
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+	"time"
+)
 
 //The SourceMap HTTP response header links generated code to a source map,
 //enabling the browser to reconstruct the original source and present the
 //reconstructed original in the debugger.
 //
-// https://mdn.io/SourceMap
+// https://mdn.io/HTTP/SourceMap
 type SourceMap struct {
 	// A relative (to the request URL) or absolute URL pointing to a source map file.
 	URL *url.URL
@@ -30,3 +35,35 @@ func (h *SourceMap) Parse(hdr string) error {
 }
 
 var _ Header = &SourceMap{}
+
+// The Age header contains the time in seconds the object has been in a proxy
+// cache.
+//
+// The Age header is usually close to zero. If it is Age: 0, it was probably
+// just fetched from the origin server; otherwise It is usually calculated as a
+// difference between the proxy's current date and the Date general header
+// included in the HTTP response.
+//
+// https://mdn.io/Age
+type Age struct {
+	// A non-negative integer, representing time in seconds the object has been
+	// in a proxy cache.
+	Cached time.Duration
+}
+
+func (h Age) Name() string {
+	return "Age"
+}
+
+func (h Age) Value() string {
+	return strconv.Itoa(int(time.Duration(h.Cached).Seconds()))
+}
+
+func (h *Age) Parse(hdr string) error {
+	age, err := strconv.Atoi(hdr)
+	if err != nil {
+		return fmt.Errorf("The value for Age must be an integer; got %s", hdr)
+	}
+	*h = Age{time.Duration(age) * time.Second}
+	return nil
+}
--- core_test.go
+++ core_test.go
@@ -3,6 +3,7 @@ package headers
 import (
 	"net/url"
 	"testing"
+	"time"
 )
 
 func TestSourceMap(t *testing.T) {
@@ -12,5 +13,12 @@ func TestSourceMap(t *testing.T) {
 	verify(t, []testcase{
 		{&SourceMap{foo}, "/foo"},
 		{&SourceMap{bar}, "https://example.com/bar"},
+	})
+}
+
+func TestAge(t *testing.T) {
+	verify(t, []testcase{
+		{&Age{}, "0"},
+		{&Age{1 * time.Hour}, "3600"},
 	})
 }
