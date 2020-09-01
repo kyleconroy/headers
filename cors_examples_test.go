17c1534a5fc39a761ddc520b357c035045eeb6d9
Kyle Conroy <kyle@conroy.org>
2020-09-01 15:29:45 -0700

    Add all examples from https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Headers

--- cors_examples_test.go
+++ cors_examples_test.go
@@ -10,6 +10,27 @@ func ExampleReverseAccessControlAllowHeaders_a_custom_header() {
 	h := AccessControlAllowHeaders{
 		Headers: []string{"X-Custom-Header"},
 	}
-	fmt.Printf("%s: %s\n", h.Name(), h.Value())
+	fmt.Printf("%s: %s", h.Name(), h.Value())
 	// Output: Access-Control-Allow-Headers: X-Custom-Header
+}
+
+// This example shows Access-Control-Allow-Headers when it specifies support
+// for multiple headers.
+func ExampleReverseAccessControlAllowHeaders_multiple_headers() {
+	h := AccessControlAllowHeaders{
+		Headers: []string{"X-Custom-Header", "Upgrade-Insecure-Requests"},
+	}
+	fmt.Printf("%s: %s", h.Name(), h.Value())
+	// Output: Access-Control-Allow-Headers: X-Custom-Header, Upgrade-Insecure-Requests
+}
+
+// Although CORS-safelisted request headers are always allowed and don't
+// usually need to be listed in Access-Control-Allow-Headers, listing them
+// anyway will circumvent the additional restrictions that apply.
+func ExampleReverseAccessControlAllowHeaders_bypassing_additional_restrictions() {
+	h := AccessControlAllowHeaders{
+		Headers: []string{"Accept"},
+	}
+	fmt.Printf("%s: %s", h.Name(), h.Value())
+	// Output: Access-Control-Allow-Headers: Accept
 }
